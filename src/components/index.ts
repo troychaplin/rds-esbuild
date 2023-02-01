c6c703fb5a52d40672cf4ac0e59019c2630e36b9
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-31 19:59:28 -0500

    Added Badge and Calendar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from './Alert/Alert'
+export * from './Badge/Badge'
 export * from './Button/Button'
+export * from './Calendar/Calendar'
 export * from './HeroIcon/HeroIcon'
 export * from './Link/Link'
